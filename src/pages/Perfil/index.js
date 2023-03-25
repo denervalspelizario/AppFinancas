b444af4d716fff7da8255241f180f91727b8799e
denervalspelizario <denervalspelizario@gmail.com>
2023-03-25 08:52:21 -0300

    update appFinancas estilizacao screen perfil

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,16 +1,22 @@
 import React, {useContext} from 'react';
 import { View, Text, Button } from 'react-native';
 
+import {Container, Nome, NewLink, NewText, Logout, LogoutText} from './styles'
+
 
 export default function Perfil() {
 
   
  return (
-   <View>
-    <Text>
-        Screen Perfil
-    </Text>
-    
-   </View> 
+   <Container>
+      <Nome> Denerval </Nome>
+      <NewLink>
+        <NewText>Registrar gastos</NewText>
+      </NewLink>
+
+      <Logout>
+        <LogoutText>Sair</LogoutText>
+      </Logout>
+   </Container>
   );
 }
